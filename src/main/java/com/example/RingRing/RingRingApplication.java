d53f78097cadb194f07e065f0179607cab4c9abb
Miguel Rodríguez <miguel19644@gmail.com>
2023-12-07 13:34:18 +0100

    first commit, created project and dependencies

---
+++ src/main/java/com/example/RingRing/RingRingApplication.java
@@ -0,0 +1,13 @@
+package com.example.RingRing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RingRingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RingRingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RingRing/RingRingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RingRing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RingRingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
