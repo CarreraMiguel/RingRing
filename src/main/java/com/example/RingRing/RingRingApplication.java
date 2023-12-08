459aaefc821ba621a49863a27f13b977036f2585
Miguel Rodríguez <miguel19644@gmail.com>
2023-12-08 11:51:56 +0100

    Created a simple endpoint function to check the conexion with port 8080 works properly (it does)

--- src/main/java/com/example/RingRing/RingRingApplication.java
+++ src/main/java/com/example/RingRing/RingRingApplication.java
@@ -2,12 +2,18 @@ package com.example.RingRing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RingRingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RingRingApplication.class, args);
 	}
-
+	@GetMapping
+	public String demo(){
+		return "Hola muchachos";
+	}
 }
