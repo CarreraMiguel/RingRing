8d6c9559c021360a3e13a7cf8b8d60a2f79d5cc4
Miguel Rodríguez <miguel19644@gmail.com>
2023-12-09 14:01:12 +0100

    Added getters and setters

--- src/main/java/com/example/RingRing/models/Post.java
+++ src/main/java/com/example/RingRing/models/Post.java
@@ -21,4 +21,43 @@ public class Post {
     @JoinColumn(name = "user_id", referencedColumnName = "Id")
     private User user;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
