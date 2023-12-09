d722e24c12ef432f798cfcedcce49a7f8ead21a5
Miguel Rodríguez <miguel19644@gmail.com>
2023-12-09 12:57:34 +0100

    Created model package and User first attributes and get/set methodes

---
+++ src/main/java/com/example/RingRing/models/User.java
@@ -0,0 +1,40 @@
+package com.example.RingRing.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "USERS")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private String name;
+    @Column
+    private String surname;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
