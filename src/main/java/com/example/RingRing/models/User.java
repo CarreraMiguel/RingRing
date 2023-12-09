8b3b46a001cebfbb04e5bfc2c621d1d4fad66619
Miguel Rodríguez <miguel19644@gmail.com>
2023-12-09 14:04:27 +0100

    Created a List of Post for each user

--- src/main/java/com/example/RingRing/models/User.java
+++ src/main/java/com/example/RingRing/models/User.java
@@ -2,24 +2,29 @@ package com.example.RingRing.models;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(name = "USERS")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private int Id;
     @Column
     private String name;
     @Column
     private String surname;
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Post> posts;
+
 
     public int getId() {
-        return id;
+        return Id;
     }
 
     public void setId(int id) {
-        this.id = id;
+        this.Id = id;
     }
 
     public String getName() {
@@ -36,5 +41,13 @@ public class User {
 
     public void setSurname(String surname) {
         this.surname = surname;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 }
