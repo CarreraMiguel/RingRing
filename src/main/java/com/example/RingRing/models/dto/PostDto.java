57857484338faadf8d2a40621e60e42e0349c0c0
Miguel Rodríguez <miguel19644@gmail.com>
2023-12-12 13:45:12 +0100

    Created DTO package and Dto class for User and Post

---
+++ src/main/java/com/example/RingRing/models/dto/PostDto.java
@@ -0,0 +1,40 @@
+package com.example.RingRing.models.dto;
+
+public class PostDto {
+    private int id;
+    private String title;
+    private String body;
+    private UserDto user;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public UserDto getUser() {
+        return user;
+    }
+
+    public void setUser(UserDto user) {
+        this.user = user;
+    }
+}
---
+++ src/main/java/com/example/RingRing/models/dto/UserDto.java
@@ -0,0 +1,47 @@
+package com.example.RingRing.models.dto;
+
+import com.example.RingRing.models.Post;
+
+import java.util.List;
+
+public class UserDto {
+    private int id;
+
+    private String name;
+
+    private String surname;
+
+    private List<Post> posts;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+}
