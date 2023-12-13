c9737684d3281896b1c5b23e6f69bdfe5c0a003d
Miguel Rodríguez <miguel19644@gmail.com>
2023-12-13 01:24:18 +0100

    Imported the correct class after fixed the pom problem

--- src/main/java/com/example/RingRing/models/dto/mappers/UserMapper.java
+++ src/main/java/com/example/RingRing/models/dto/mappers/UserMapper.java
@@ -1,5 +1,10 @@
 package com.example.RingRing.models.dto.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
 @Mapper
 public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
 }
