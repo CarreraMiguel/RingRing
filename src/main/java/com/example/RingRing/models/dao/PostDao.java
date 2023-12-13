0f5bca1771be2875f719a39e152ee7c521d9d976
Miguel Rodríguez <miguel19644@gmail.com>
2023-12-13 12:30:48 +0100

    Renamed files to follow the project denomination files coherence.

--- src/main/java/com/example/RingRing/models/dao/PostDao.java
+++ src/main/java/com/example/RingRing/models/dao/PostDao.java
@@ -3,5 +3,5 @@ package com.example.RingRing.models.dao;
 import com.example.RingRing.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface postDao extends JpaRepository<Post, Integer> {
+public interface PostDao extends JpaRepository<Post, Integer> {
 }
--- src/main/java/com/example/RingRing/models/dao/UserDao.java
+++ src/main/java/com/example/RingRing/models/dao/UserDao.java
@@ -3,5 +3,5 @@ package com.example.RingRing.models.dao;
 import com.example.RingRing.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface userDao  extends JpaRepository<User,Integer> {
+public interface UserDao extends JpaRepository<User,Integer> {
 }
