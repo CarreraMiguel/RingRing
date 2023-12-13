b86eb2f7aa26f43cac2affbb443c2925639a4721
Miguel Rodríguez <miguel19644@gmail.com>
2023-12-13 12:21:08 +0100

    Created mapper for Users

--- src/main/java/com/example/RingRing/models/dto/mappers/UserMapper.java
+++ src/main/java/com/example/RingRing/models/dto/mappers/UserMapper.java
@@ -1,10 +1,19 @@
 package com.example.RingRing.models.dto.mappers;
 
+import com.example.RingRing.models.User;
+import com.example.RingRing.models.dto.UserDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper
 public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+    UserDto toDTO (User user);
+
+    List<UserDto> toDTOList (List<User> users);
+
+    User toEntity (UserDto userDto);
 
 }
