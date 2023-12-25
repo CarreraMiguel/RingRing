5a8d3c3fa0851e32eb18ba7a600af727f399afdf
Miguel Rodríguez <miguel19644@gmail.com>
2023-12-25 14:12:44 +0100

    Created CRUD methodes for Post Controller

--- src/main/java/com/example/RingRing/controller/PostController.java
+++ src/main/java/com/example/RingRing/controller/PostController.java
@@ -1,4 +1,46 @@
 package com.example.RingRing.controller;
 
+import com.example.RingRing.api.iPostService;
+import com.example.RingRing.models.dto.PostDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/posts")
 public class PostController {
+    @Autowired
+    iPostService postService;
+
+    @GetMapping (value = "/testPostController")
+    public String test(){
+        return ("Post controller works!");
+    }
+
+    @GetMapping (value = "/getAll")
+    public List<PostDto> queryAllPosts(){
+        return this.postService.queryAllPosts();
+    }
+
+    @PostMapping (value = "/get")
+    public PostDto queryPost(@RequestBody PostDto post){
+        return this.postService.queryPost(post);
+    }
+
+    @PostMapping (value = "/add")
+    public int insertPost(@RequestBody PostDto post){
+        return this.postService.insertPost(post);
+    }
+
+    @PutMapping (value = "/update")
+    public int updatePost(@RequestBody PostDto post){
+        return this.postService.updatePost(post);
+    }
+
+    @DeleteMapping (value = "/delete")
+    public int deletePost(@RequestBody PostDto post){
+        return this.postService.deletePost(post);
+    }
+
 }
