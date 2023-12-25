bbe3de229676a0892fd4976514b2165d33f34b6c
Miguel Rodríguez <miguel19644@gmail.com>
2023-12-25 14:28:27 +0100

    Getters and Setters

--- src/main/java/com/example/RingRing/models/Commentary.java
+++ src/main/java/com/example/RingRing/models/Commentary.java
@@ -17,4 +17,36 @@ public class Commentary {
     @ManyToOne
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
