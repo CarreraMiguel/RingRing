473512b64f38571d71ab18541c6af236e51eb083
Miguel Rodríguez <miguel19644@gmail.com>
2023-12-25 14:26:45 +0100

    Renamed class from "Comentarie" to "Commentary" for more English accuracy.

--- src/main/java/com/example/RingRing/models/Commentary.java
+++ src/main/java/com/example/RingRing/models/Commentary.java
@@ -2,14 +2,18 @@ package com.example.RingRing.models;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "COMENTARIES")
-public class Comentaries {
+public class Commentary {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @Column
-    String body;
+    @Column (columnDefinition = "TEXT")
+    private String body;
+    @Column (name = "created_date")
+    private LocalDateTime createdDate;
     @ManyToOne
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
