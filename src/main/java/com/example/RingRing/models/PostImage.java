d2ef06a9dbf3383680d0a31faa3095d67ec84f02
Miguel Rodríguez <miguel19644@gmail.com>
2023-12-26 12:06:21 +0100

    Created a Post Image, initial atributes and getter/setters

---
+++ src/main/java/com/example/RingRing/models/PostImage.java
@@ -0,0 +1,21 @@
+package com.example.RingRing.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "POST_IMAGE")
+public class PostImage extends Post {
+
+    @Column
+    private String dimension;
+
+    public String getDimension() {
+        return dimension;
+    }
+
+    public void setDimension(String dimension) {
+        this.dimension = dimension;
+    }
+}
