d3d5169e781abb82f0be1cf7442910fb59fd1911
Miguel Rodríguez <miguel19644@gmail.com>
2023-12-26 12:13:31 +0100

    Created my Data transfer Object for PostVideo Entity

---
+++ src/main/java/com/example/RingRing/models/dto/PostVideoDto.java
@@ -0,0 +1,22 @@
+package com.example.RingRing.models.dto;
+
+public class PostVideoDto {
+    private int quality;
+    private String duration;
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+}
