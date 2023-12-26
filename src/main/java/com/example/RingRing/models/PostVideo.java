af2a240c279f298e8fd674c53824bc45a49e14e8
Miguel Rodríguez <miguel19644@gmail.com>
2023-12-26 12:06:42 +0100

    Created a Post Video with initial atributes and getter/setters

---
+++ src/main/java/com/example/RingRing/models/PostVideo.java
@@ -0,0 +1,30 @@
+package com.example.RingRing.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "POST_VIDEO")
+public class PostVideo extends Post{
+    @Column
+    private int quality;
+    @Column
+    private String duration;
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+}
