680cfb616df11b75fdcd916c59eb05c2fb43d28c
Miguel Rodríguez <miguel19644@gmail.com>
2023-12-26 12:33:36 +0100

    Created DAO Interface for PostImage

---
+++ src/main/java/com/example/RingRing/models/dao/PostImageDao.java
@@ -0,0 +1,7 @@
+package com.example.RingRing.models.dao;
+
+import com.example.RingRing.models.PostImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostImageDao extends JpaRepository<PostImage, Integer> {
+}
