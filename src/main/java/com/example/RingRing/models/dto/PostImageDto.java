b078ffde13d4d11ae3224e124bb90b0488674d87
Miguel Rodríguez <miguel19644@gmail.com>
2023-12-26 12:13:23 +0100

    Created my Data transfer Object for PostImage Entity

---
+++ src/main/java/com/example/RingRing/models/dto/PostImageDto.java
@@ -0,0 +1,14 @@
+package com.example.RingRing.models.dto;
+
+public class PostImageDto {
+
+    private String dimension;
+
+    public String getDimension() {
+        return dimension;
+    }
+
+    public void setDimension(String dimension) {
+        this.dimension = dimension;
+    }
+}
