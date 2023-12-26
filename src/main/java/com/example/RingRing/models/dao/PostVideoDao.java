d609c055f4c7209588e0e82f662ac8238ff4dd85
Miguel Rodríguez <miguel19644@gmail.com>
2023-12-26 12:33:44 +0100

    Created DAO Interface for PostVideo

---
+++ src/main/java/com/example/RingRing/models/dao/PostVideoDao.java
@@ -0,0 +1,7 @@
+package com.example.RingRing.models.dao;
+
+import com.example.RingRing.models.PostVideo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostVideoDao extends JpaRepository<PostVideo, Integer> {
+}
