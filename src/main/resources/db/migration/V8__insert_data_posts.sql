38f89a40eabd85f6495e82f55197aeca4833e257
Miguel Rodríguez <miguel19644@gmail.com>
2024-01-03 12:28:15 +0100

    Inserted values into POSTS table via mockaroo

---
+++ src/main/resources/db/migration/V8__insert_data_posts.sql
@@ -0,0 +1,11 @@
+INSERT INTO Posts (title, body) VALUES
+('Post 1 Title', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at ultricies justo. Quisque feugiat, arcu in laoreet tempus, urna metus laoreet justo, eu consequat elit tellus at nisl.'),
+('Post 2 Title', 'Vivamus nec justo sit amet sem tincidunt vestibulum. Sed hendrerit risus vitae semper venenatis. Integer facilisis dui vel magna eleifend, nec vestibulum augue dapibus.'),
+('Post 3 Title', 'Maecenas vehicula odio et venenatis suscipit. Donec fermentum arcu vel sapien consectetur, vitae aliquet justo consectetur. Nullam tincidunt tincidunt mauris, in ultrices nisl gravida ac.'),
+('Post 4 Title', 'Proin rhoncus velit ut lectus hendrerit fringilla. Suspendisse ut neque nec justo vestibulum tristique. Integer vel malesuada augue.'),
+('Post 5 Title', 'Sed efficitur turpis ut lacus vehicula, vel scelerisque urna sodales. Nullam bibendum metus in ultricies fermentum. Phasellus et tellus vel orci tincidunt accumsan.'),
+('Post 6 Title', 'Ut euismod nisl vel orci pellentesque eleifend. Fusce eget augue ac justo euismod vehicula. Aenean facilisis tortor vel neque dapibus fermentum.'),
+('Post 7 Title', 'Pellentesque sed ligula vel ex sodales bibendum. Quisque vel nulla vel ex lacinia tristique ac eu ex. Curabitur non tortor ut lectus ullamcorper cursus.'),
+('Post 8 Title', 'Duis ac diam nec ex fringilla tincidunt eu id neque. Suspendisse ullamcorper facilisis leo, vitae vulputate arcu dapibus in.'),
+('Post 9 Title', 'Nam id ex eu lacus pharetra tristique. Curabitur tincidunt, felis vel rhoncus vulputate, eros elit tincidunt libero, vel fermentum urna tortor id nunc.'),
+('Post 10 Title', 'Integer eu neque vel tortor convallis ultricies. In hac habitasse platea dictumst. Nullam condimentum, felis eu convallis luctus, dui urna dapibus sapien, vel tincidunt nulla justo id lacus.');
