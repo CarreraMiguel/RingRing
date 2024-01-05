074d184757b0a99e61f8185da69d79f3d4020e0c
Miguel Rodríguez <miguel19644@gmail.com>
2024-01-05 11:43:25 +0100

    Inserted data into post videos table

---
+++ src/main/resources/db/migration/V12__insert_data_post_video.sql
@@ -0,0 +1,21 @@
+INSERT INTO POST_VIDEOS (title, body, created_date, quality, duration)
+VALUES
+  ('Laughing Out Loud', 'Why dont scientists trust atoms? Because they make up everything!', '2023-01-06', 1080, '15'),
+
+  ('The Ticklish Chicken', 'Why did the chicken join a comedy club? Because it had a great sense of humor!', '2023-02-15', 1080, '15 seconds'),
+
+  ('Penguin Jokes', 'What do you call a penguin in the desert? Lost!', '2023-03-22', 1080, '8 seconds'),
+
+  ('Coffee vs. Tea', 'Why did the coffee file a police report? It got mugged!', '2023-04-11', 1080, '12 seconds'),
+
+  ('The Musical Cat', 'Why was the cat sitting on the computer? It wanted to keep an eye on the mouse!', '2023-05-30', 720, '18 seconds'),
+
+  ('Lost Keys', 'Why did the scarecrow win an award? Because he was outstanding in his field!', '2023-06-18', 480, '9 seconds'),
+
+  ('Ice Cream Logic', 'Why did the ice cream truck break down? Because it had too many licks!', '2023-07-25', 1080, '14 seconds'),
+
+  ('The Invisible Man', 'Why dont you ever see elephants hiding in trees? Because they are so good at it!', '2023-08-14', 2000, '20 seconds'),
+
+  ('Dancing Shoes', 'Why dont scientists trust atoms? Because they make up everything!', '2023-09-09', 1080, '11 seconds'),
+
+  ('Tech Support Parrot', 'Why did the parrot go to tech support? It had tweet problems!', '2023-10-17', 1080, '16 seconds');
