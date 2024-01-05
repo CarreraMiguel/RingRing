ad5bc71c815dea9bdb984d56961d747bc1a9a50c
Miguel Rodríguez <miguel19644@gmail.com>
2024-01-05 11:33:24 +0100

    Inserted Data into Post Images

---
+++ src/main/resources/db/migration/V11__insert_data_post_image.sql
@@ -0,0 +1,11 @@
+INSERT INTO POST_IMAGES (title, body, created_date, dimension) VALUES
+  ('Summer', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', '2023-10-04', '500x300'),
+  ('Bike', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', '2024-01-04', '600x400'),
+  ('Sunset', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', '2023-02-04', '700x500'),
+  ('Midnight', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', '2023-03-08', '550x350'),
+  ('Beach Time', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', '2023-06-04', '750x450'),
+  ('Programming', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', '2023-09-20', '650x350'),
+  ('Winning', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', '2023-08-19', '700x400'),
+  ('On the gym', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', '2023-07-14', '600x300'),
+  ('Flexing', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', '2023-12-24', '850x550'),
+  ('Feeling cute today', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', '2023-08-18', '900x600');
