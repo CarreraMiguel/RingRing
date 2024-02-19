29eba76969cfd1553131dd02ad01377eb117b464
Miguel Rodríguez <miguel19644@gmail.com>
2024-02-19 16:32:13 +0100

    Created listPostImages methode. Also created listPostVideos methode.

--- src/main/java/com/example/RingRing/api/IUserService.java
+++ src/main/java/com/example/RingRing/api/IUserService.java
@@ -1,6 +1,8 @@
 package com.example.RingRing.api;
 
 import com.example.RingRing.models.dto.PostDto;
+import com.example.RingRing.models.dto.PostImageDto;
+import com.example.RingRing.models.dto.PostVideoDto;
 import com.example.RingRing.models.dto.UserDto;
 
 import java.util.List;
@@ -15,4 +17,6 @@ public interface IUserService {
     int removeFriend(UserDto userDto);
     List <UserDto> listFriends(UserDto userDto);
     List <PostDto> listPosts(UserDto userDto);
+    List<PostImageDto> listPostImages(UserDto userDto);
+    List<PostVideoDto> listPostVideos(UserDto userDto);
 }
