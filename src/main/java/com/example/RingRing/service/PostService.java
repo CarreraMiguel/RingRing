8f60c81aa904c48c739e659c44de3534ced0b836
Miguel Rodríguez <miguel19644@gmail.com>
2024-03-12 11:50:50 +0100

    Refactorized CreateTextPost methode to work by using Post objects and not Json objects.

--- src/main/java/com/example/RingRing/service/PostService.java
+++ src/main/java/com/example/RingRing/service/PostService.java
@@ -54,13 +54,11 @@ public class PostService implements IPostService {
     @Override
     public PostDto createTextPost(PostDto postDto){
         Post post = PostMapper.INSTANCE.toEntity(postDto);
-        JSONObject jsonObject = new JSONObject();
         Scanner leer = new Scanner(System.in);
         while(true){
             System.out.println("Título: ");
             String title = leer.nextLine();
             if(title != null && !title.isEmpty() && title.matches("^[a-zA-Z0-9 ]+$")){
-                jsonObject.put("title", title);
                 post.setTitle(title);
                 break;
             } else {
@@ -71,7 +69,6 @@ public class PostService implements IPostService {
             System.out.println("Contenido del Post:");
             String body = leer.nextLine();
             if(body != null && !body.isEmpty() && body.matches("^[a-zA-Z0-9 ]+$")){
-                jsonObject.put("body", body);
                 post.setBody(body);
                 break;
             } else {
